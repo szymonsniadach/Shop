bdfb9e00a4870e167a63eea48739a8a15fa34c11
Szymon Ś <sz.sniadach@gmail.com>
2019-08-30 00:20:01 +0200

    - add reset styles - add some utilities scss - create header component

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {httpInterceptorProviders} from "./core/interceptors/interceptors";
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
