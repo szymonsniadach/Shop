53e24e6cb33b55c6d550e061d3e4dfc0a8c167b1
Szymon <sz.sniadach@gmail.com>
2019-09-05 01:17:58 +0200

    - add style file: display, sizing, spacing - add some style in flex - improve grid style - add class to font-size - add two boxes with icons: login and cart

--- shop/src/app/core/header/header.component.ts
+++ shop/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faShoppingCart, faUser} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  public userIcon = faUser;
+  public cartIcon = faShoppingCart;
 
   constructor() { }
 
