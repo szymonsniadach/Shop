5361c36d10c2d03f2d2a4ef2fba986039ee98616
Szymon <sz.sniadach@gmail.com>
2019-10-14 01:03:18 +0200

    fix import in url.interceptor file

--- shop/src/app/core/interceptors/url.interceptor.ts
+++ shop/src/app/core/interceptors/url.interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import {environment} from '@env/environment';
 
 @Injectable()
 export class UrlInterceptor implements HttpInterceptor {
